96170985604d8f231feebdf37f6ab64812885c62
Sunil <sunil.kumar@imperial.ac.uk>
2018-10-16 17:40:29 +0100

    Initial commit - need to fix this at some point - Unable to instantiate class for class='OPT_main', priority=0.0, enabled=true, pluginType=MenuPlugin

---
+++ OPT_MM2/src/OPT_UI/OPT_main.java
@@ -0,0 +1,65 @@
+package OPT_UI;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fogim
+ */
+
+import com.google.common.eventbus.Subscribe;
+import javax.swing.JFrame;
+import org.micromanager.MenuPlugin;
+import org.micromanager.Studio;
+import org.scijava.plugin.Plugin;
+import org.scijava.plugin.SciJavaPlugin;
+
+@Plugin(type = MenuPlugin.class)
+public class OPT_main  implements MenuPlugin, SciJavaPlugin{
+    //Name for the plugin
+    public static final String menuName = "OPT for MM2";
+    private Studio gui_;    
+    public static JFrame frame_;
+    
+    @Override
+    public String getSubMenu() {
+        return("Acquisition Tools");
+    }
+
+    @Override
+    public void onPluginSelected() {
+        //System.out.println("Tada!");
+        frame_ = new OPT_UI.OPT_hostframe(gui_);
+        frame_.show();
+    }
+
+    @Override
+    public void setContext(Studio studio) {
+        gui_ = studio;
+    }
+
+    @Override
+    public String getName() {
+        return menuName;
+    }
+
+    @Override
+    public String getHelpText() {
+        return("Sorry!");
+    }
+
+    @Override
+    public String getVersion() {
+        return ("0.0.1");
+    }
+
+    @Override
+    public String getCopyright() {
+        return ("Copyright Imperial College London[2018]");
+    }
+    
+}
